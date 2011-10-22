2c12d4520fefbecf3991b5a87a4f714ed7520a6b
zhangjiji <programmer.zhangjiji@gmail.com>
2011-10-23 00:57:09 +0800

    Add constants and protocol.

---
+++ CVS-Tycoon/CommonProtocols.h
@@ -0,0 +1,63 @@
+//
+//  CommonProtocols.h
+//  CVS-Tycoon
+//
+//  Created by Zhang Zhe on 9/27/11.
+//  Copyright 2011 AvalonGameArt. All rights reserved.
+//
+
+#ifndef CVSTYCOON_CommonProtocols_h
+#define CVSTYCOON_CommonProtocols_h
+
+typedef enum {
+    kStateSpawning,
+    kStateIdle,
+    kStateCrouching,
+    kStateStandingUp,
+    kStateWalking,
+    kStateAttacking,
+    kStateJumping,
+    kStateBreathing,
+    kStateTakingDamage,
+    kStateDead,
+    kStateTraveling,
+    kStateRotating,
+    kStateDrilling,
+    kStateAfterJumping
+} CharacterStates;
+
+typedef enum {
+    kObjectTypeNone,
+    kPowerUpTypeHealth,
+    kPowerUpTypeMallet,
+    kEnemyTypeRadarDish,
+    kEnemyTypeSpaceCargoShip,
+    kEnemyTypeAlienRobot,
+    kEnemyTypePhaser,
+    kVikingType,
+    kSkullType,
+    kRockType,
+    kMeteorType,
+    kFrozenVikingType,
+    kIceType,
+    kLongBlockType,
+    kCartType,
+    kSpikesType,
+    kDiggerType,
+    kGroundType
+} GameObjectType;
+
+@protocol GameplayLayerDelegate
+
+-(void) createObjectOfType:(GameObjectType)objectType
+                withHealth:(int)initialHealth
+                atLocation:(CGPoint)spawnLocation
+                withZValue:(int)ZValue;
+
+-(void) createPhaserWithDirection:(PhaserDirection)PhaserDirection
+                      andPosition:(CGPoint)spawnPosition;
+
+@end
+
+
+#endif
---
+++ CVS-Tycoon/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  CVS-Tycoon
+//
+//  Created by Zhang Zhe on 9/25/11.
+//  Copyright 2011 AvalonGameArt. All rights reserved.
+//
+
+#ifndef CVSTYCOON_Constants_h
+#define CVSTYCOON_Constants_h
+
+
+#endif
