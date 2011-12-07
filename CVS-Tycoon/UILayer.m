dbd3bd5b3645413ef831a884a0feb46ee53aec65
zhangjiji <programmer.zhangjiji@gmail.com>
2011-12-08 07:42:09 +0800

    +[ui] add ui layer for HUD, menus.

---
+++ CVS-Tycoon/UILayer.m
@@ -0,0 +1,13 @@
+//
+//  UILayer.m
+//  CVS-Tycoon
+//
+//  Created by ZHANG ZHE on 12/8/11.
+//  Copyright (c) 2011 AvalonGameArt. All rights reserved.
+//
+
+#import "UILayer.h"
+
+@implementation UILayer
+
+@end
